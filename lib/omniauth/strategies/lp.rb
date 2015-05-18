ea59689863b16c3f34d1f707f51ed18886f3c1da
Steve Dalton <steve@secluded.io>
2015-05-18 11:31:09 +1000

    DEV-2499 Support new LP OAUTH2 user graph format

--- lib/omniauth/strategies/lp.rb
+++ lib/omniauth/strategies/lp.rb
@@ -1,44 +1,35 @@
 require 'omniauth-oauth2'
 
 module OmniAuth
-  module Strategies
-    class Lp < OmniAuth::Strategies::OAuth2
-      # Give your strategy a name.
-      option :name, "lp"
+    module Strategies
+        class Lp < OmniAuth::Strategies::OAuth2
+            # Give your strategy a name.
+            option :name, "lp"
 
-      # This is where you pass the options you would pass when
-      # initializing your consumer from the OA  uth gem.
-      option :client_options, {
-                                :site => "http://localhost:4000",
-                                :authorize_url => '/oauth/authorize'
-                            }
+            # This is where you pass the options you would pass when
+            # initializing your consumer from the OA  uth gem.
+            option :client_options, {
+                                      :site => "http://localhost:4000"
+                                  }
 
-      # These are called after authentication has succeeded. If
-      # possible, you should try to set the UID without making
-      # additional calls (if the user id is returned with the token
-      # or as a URI parameter). This may not be possible with all
-      # providers.
+            # These are called after authentication has succeeded. If
+            # possible, you should try to set the UID without making
+            # additional calls (if the user id is returned with the token
+            # or as a URI parameter). This may not be possible with all
+            # providers.
 
-      uid do
-        raw_info['student']['id']
-      end
+            uid do
+                @raw_info['uid']
+            end
 
-      info do
-        {
-            :email => raw_info['student']['parent_email'],
-            :first_name => raw_info['student']['first'],
-            :last_name => raw_info['student']['last'],
-            :username => raw_info['student']['login'],
-            :date_of_birth => raw_info['student']['dob'],
-            :student_class_id => raw_info['student']['klass_id']
-        }
-      end
+            info do
+                @raw_info
+            end
 
-      def raw_info
-        # TODO: Need to also add teacher_id, class_id, school_id and anything else we need post login.
-        @raw_info ||= access_token.get('/api/me.json').parsed
-      end
+            def raw_info
+                @raw_info ||= access_token.get('/api/me.json').parsed
+            end
+        end
     end
-  end
 end
 
